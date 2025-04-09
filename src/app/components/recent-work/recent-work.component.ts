531b943ca640de9092dd763fafc78a464b8b88a6
Nicolas Zozor <nick.zozor@gmail.com>
2025-04-09 21:28:59 +0100

    Started with desktop english version

--- src/app/components/recent-work/recent-work.component.ts
+++ src/app/components/recent-work/recent-work.component.ts
@@ -16,19 +16,177 @@ export class RecentWorkComponent implements AfterViewInit{
   recentWork = [
     {
       id: 2,
-      title: 'Project 1',
-      imgSrc: 'assets/day-race-culture-house.jpg'
+      title: 'Shop Beirut',
+      activeImageId: 1, // Track active image
+      imgSrc: 'assets/work/shop-beirut-culture-house/main-shop-beirut-culture-house.jpg',
+      carouselImages: [
+        {
+          id: 1,
+          imgSrc: 'assets/work/shop-beirut-culture-house/main-shop-beirut-culture-house.jpg'
+        },
+        {
+          id: 2,
+          imgSrc: 'assets/work/shop-beirut-culture-house/shop-beirut-1-culture-house.jpg'
+        },
+        {
+          id: 3,
+          imgSrc: 'assets/work/shop-beirut-culture-house/shop-beirut-2-culture-house.jpg'
+        }
+      ]
     },
     {
       id: 3,
-      title: 'Project 2',
-      imgSrc: 'assets/fashion-futures-culture-house.jpg'
+      title: 'Intercontinental Hotels',
+      activeImageId: 1, imgSrc: 'assets/work/intercontinental-hotels-culture-house/main-intercontinental-hotels-culture-house.jpg',
+      carouselImages: [
+        {
+          id: 1,
+          activeImageId: 1, imgSrc: 'assets/work/intercontinental-hotels-culture-house/main-intercontinental-hotels-culture-house.jpg'
+        },
+        {
+          id: 2,
+          imgSrc: 'assets/work/intercontinental-hotels-culture-house/intercontinental-hotels-1-culture-house.jpg'
+        },
+        {
+          id: 3,
+          imgSrc: 'assets/work/intercontinental-hotels-culture-house/intercontinental-hotels-2-culture-house.jpg'
+        }
+      ]
     },
     {
       id: 4,
-      title: 'Project 3',
-      imgSrc: 'assets/ghana-edition-culture-house.jpg'
-    }];
+      title: 'Ghana Edition',
+      activeImageId: 1, imgSrc: 'assets/work/ghana-edition-culture-house/main-ghana-edition-culture-house.jpg',
+      carouselImages: [
+        {
+          id: 1,
+          imgSrc: 'assets/work/ghana-edition-culture-house/main-ghana-edition-culture-house.jpg'
+        },
+        {
+          id: 2,
+          imgSrc: 'assets/work/ghana-edition-culture-house/ghana-edition-1-culture-house.jpg'
+        },
+        {
+          id: 3,
+          imgSrc: 'assets/work/ghana-edition-culture-house/ghana-edition-2-culture-house.jpg'
+        }
+      ]
+    },
+    {
+      id: 5,
+      title: 'The Hidden Kaleidoscope',
+      activeImageId: 1, imgSrc: 'assets/work/the-hidden-kaleidoscope-culture-house/main-the-hidden-kaleidoscope-culture-house.jpg',
+      carouselImages: [
+        {
+          id: 1,
+          imgSrc: 'assets/work/the-hidden-kaleidoscope-culture-house/main-the-hidden-kaleidoscope-culture-house.jpg'
+        },
+        {
+          id: 2,
+          imgSrc: 'assets/work/the-hidden-kaleidoscope-culture-house/the-hidden-kaleidoscope-1-culture-house.jpg'
+        },
+        {
+          id: 3,
+          imgSrc: 'assets/work/the-hidden-kaleidoscope-culture-house/the-hidden-kaleidoscope-2-culture-house.jpg'
+        }
+      ]
+    },
+    {
+      id: 6,
+      title: 'Turquoise Mountain Trust',
+      activeImageId: 1, imgSrc: 'assets/work/turquoise-mountain-trust-culture-house/main-turquoise-mountain-trust-culture-house.jpg',
+      carouselImages: [
+        {
+          id: 1,
+          imgSrc: 'assets/work/turquoise-mountain-trust-culture-house/main-turquoise-mountain-trust-culture-house.jpg'
+        },
+        {
+          id: 2,
+          imgSrc: 'assets/work/turquoise-mountain-trust-culture-house/turquoise-mountain-trust-1-culture-house.jpg'
+        },
+        {
+          id: 3,
+          imgSrc: 'assets/work/turquoise-mountain-trust-culture-house/turquoise-mountain-trust-2-culture-house.jpg'
+        }
+      ]
+    },
+    {
+      id: 7,
+      title: 'A Day At The Race',
+      activeImageId: 1, imgSrc: 'assets/work/a-day-at-the-race-culture-house/main-a-day-at-the-race-culture-house.jpg',
+      carouselImages: [
+        {
+          id: 1,
+          imgSrc: 'assets/work/a-day-at-the-race-culture-house/main-a-day-at-the-race-culture-house.jpg'
+        },
+        {
+          id: 2,
+          imgSrc: 'assets/work/a-day-at-the-race-culture-house/a-day-at-the-race-1-culture-house.jpg'
+        },
+        {
+          id: 3,
+          imgSrc: 'assets/work/a-day-at-the-race-culture-house/a-day-at-the-race-2-culture-house.jpg'
+        }
+      ]
+    },
+    {
+      id: 8,
+      title: 'Tribe',
+      activeImageId: 1, imgSrc: 'assets/work/tribe-culture-house/main-tribe-culture-house.jpg',
+      carouselImages: [
+        {
+          id: 1,
+          imgSrc: 'assets/work/tribe-culture-house/main-tribe-culture-house.jpg'
+        },
+        {
+          id: 2,
+          imgSrc: 'assets/work/tribe-culture-house/tribe-1-culture-house.jpg'
+        },
+        {
+          id: 3,
+          imgSrc: 'assets/work/tribe-culture-house/tribe-2-culture-house.jpg'
+        }
+      ]
+    },
+    {
+      id: 9,
+      title: 'Fashion Futuress',
+      activeImageId: 1, imgSrc: 'assets/work/fashion-futures-culture-house/main-fashion-futures-culture-house.jpg',
+      carouselImages: [
+        {
+          id: 1,
+          imgSrc: 'assets/work/fashion-futures-culture-house/main-fashion-futures-culture-house.jpg'
+        },
+        {
+          id: 2,
+          imgSrc: 'assets/work/fashion-futures-culture-house/fashion-futures-1-culture-house.jpg'
+        },
+        {
+          id: 3,
+          imgSrc: 'assets/work/fashion-futures-culture-house/fashion-futures-2-culture-house.jpg'
+        }
+      ]
+    },
+    {
+      id: 10,
+      title: 'Souq Waqif',
+      activeImageId: 1, imgSrc: 'assets/work/souq-waqif-culture-house/main-souq-waqif-culture-house.jpg',
+      carouselImages: [
+        {
+          id: 1,
+          imgSrc: 'assets/work/souq-waqif-culture-house/main-souq-waqif-culture-house.jpg'
+        },
+        {
+          id: 2,
+          imgSrc: 'assets/work/souq-waqif-culture-house/souq-waqif-1-culture-house.jpg'
+        },
+        {
+          id: 3,
+          imgSrc: 'assets/work/souq-waqif-culture-house/souq-waqif-2-culture-house.jpg'
+        }
+      ]
+    }
+  ];
 
   ngAfterViewInit() {
     this.setupIntersectionObserver();
