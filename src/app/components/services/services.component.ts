1d936539ea1207289346b7fc945058ddb1ba0293
Nicolas Zozor <nick.zozor@gmail.com>
2025-04-13 10:14:59 +0100

    start some design review and fix some rtl issues

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -14,18 +14,17 @@ export class ServicesComponent {
 // Define service items with translation keys
   // Column 1 services
   columnOneServices = [
-    { id: 1, name: 'SERVICES.ITEMS.USER_JOURNEY' },
+    { id: 1, name: 'SERVICES.ITEMS.UX_DESIGN' },
     { id: 2, name: 'SERVICES.ITEMS.SOCIAL_MEDIA' },
     { id: 3, name: 'SERVICES.ITEMS.BROCHURES' },
-    { id: 4, name: 'SERVICES.ITEMS.TV_CONCEPTS' },
-    { id: 5, name: 'SERVICES.ITEMS.CONTENT_CREATION' }
+    { id: 4, name: 'SERVICES.ITEMS.TV_SHOWS' },
+    { id: 5, name: 'SERVICES.ITEMS.CONTENT' }
   ];
-
   // Column 2 services
   columnTwoServices = [
     { id: 6, name: 'SERVICES.ITEMS.GIFTS' },
     { id: 7, name: 'SERVICES.ITEMS.MAGAZINES' },
-    { id: 8, name: 'SERVICES.ITEMS.PRINTED_BOOKS' },
+    { id: 8, name: 'SERVICES.ITEMS.BOOKS' },
     { id: 9, name: 'SERVICES.ITEMS.EXHIBITIONS' }
   ];
 }
